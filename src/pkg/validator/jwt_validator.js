8f834632f69e35fca513110f985f9bbb3e349caa
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-08-16 21:59:41 +0100

    fix: extend JWT token expiration time to 10 hours and improve error logging

--- src/pkg/validator/jwt_validator.js
+++ src/pkg/validator/jwt_validator.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken';
+import 'dotenv/config.js'
 
 export const JWT_KEY = process.env.JWT_KEY
 
 export default function CreateJwtToken(user_id, user_name, role) {
-    return jwt.sign({user_id: user_id, user_name, role}, JWT_KEY, {expiresIn: '2h'});
+    return jwt.sign({user_id: user_id, user_name, role}, JWT_KEY, {expiresIn: '10h'});
 }
 
 export function VerifyJwtToken(token) {
     try {
         return jwt.verify(token, JWT_KEY);
     } catch (err) {
+        console.error("JWT verification failed:", err);
         return null
     }
 }
