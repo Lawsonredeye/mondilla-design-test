234bc056a72d939d6147fd45c947ada4368e5a6c
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-08-16 18:57:11 +0100

    feat: add error handling for JWT token creation, load environment variables using the dotenv library

--- src/pkg/validator/jwt_validator.js
+++ src/pkg/validator/jwt_validator.js
@@ -1,9 +1,15 @@
 import jwt from 'jsonwebtoken';
+import 'dotenv/config'
 
 export const JWT_KEY = process.env.JWT_KEY
 
 export default function CreateJwtToken(user_id, user_name, role) {
+    try {
     return jwt.sign({user_id: user_id, user_name, role}, JWT_KEY, {expiresIn: '2h'});
+    } catch (e) {
+        console.log("failed to load environment variable to console: ", e)
+        return null
+    }
 }
 
 export function VerifyJwtToken(token) {
