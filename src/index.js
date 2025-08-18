86e1e2b2ae428758401b9f90618baa3f31bbd849
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-08-18 10:24:16 +0100

    feat: add error handling middleware and protect project routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './controller/userController.js'
 import projectRouter from './controller/projectController.js';
 import protectedAuth from "./controller/auth.js";
+import errorNotFound from "./controller/middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use("/auth", userRouter)
 app.get("/ping", (req, res) => {
     res.json({"message": "Welcome to the User Management API", "status": "success"});
 })
-app.use(protectedAuth)
-app.use("/projects", projectRouter)
+
+app.use("/projects", protectedAuth, projectRouter)
+app.use(errorNotFound);
 export default app;
